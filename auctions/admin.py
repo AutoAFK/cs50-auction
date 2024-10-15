b272685ffdeab35936a1233afa2895373a784ba2
AutoAFK <ranhaim85@gmail.com>
2024-10-15 17:41:37 +0300

    created models and added to admin site

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Auction,Bid,Comment,Item,User
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Auction)
+admin.site.register(Bid)
+admin.site.register(Comment)
+admin.site.register(Item)
